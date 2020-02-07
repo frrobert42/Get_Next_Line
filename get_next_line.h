4511e7002ca8d97ed1fa3d700ac7630cb14d88c2
frrobert <frrobert@student.42.fr>
2020-02-07 15:21:10 +0100

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frrobert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/19 08:58:24 by frrobert          #+#    #+#             */
+/*   Updated: 2018/11/22 10:00:50 by frrobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 32
+
+int		get_next_line(int const fd, char **line);
+
+#endif
